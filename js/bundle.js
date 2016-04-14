6dc2e455cf49c5cf2804f76e2f00f6d2e4a0a07f
App Academy Student <student@appacademy.io>
2016-04-13 17:22:11 -0700

    Added rendering and step functions

--- js/bundle.js
+++ js/bundle.js
@@ -49,6 +49,12 @@
 	$(function() {
 	  var view = new SnakeView($("#snake-game"));
 	  view.setup();
+	  view.eventBinding();
+	  view.render();
+	
+	  setInterval(function(){
+	    view.step();
+	  }, 500);
 	});
 
 
@@ -127,6 +133,13 @@
 	  this.board = new Board(dimX, dimY);
 	}
 	
+	SnakeView.KEYMAP = {
+	  "37": "W",
+	  "38": "N",
+	  "39": "E",
+	  "40": "S",
+	};
+	
 	SnakeView.prototype.setup = function() {
 	  for (var i = 0; i < this.board.dimY; i++) {
 	    var $row = $("<ul></ul>");
@@ -141,6 +154,29 @@
 	
 	    this.$rootEl.append($row);
 	  }
+	};
+	
+	SnakeView.prototype.eventBinding = function () {
+	  var self = this;
+	
+	  $("body").on("keydown", function(event){
+	    self.board.snake.turn(SnakeView.KEYMAP[event["keyCode"]]);
+	  });
+	};
+	
+	SnakeView.prototype.render = function() {
+	  $(".square").removeClass("snake-body");
+	
+	  var segments = this.board.snake.segments;
+	  for (var i = 0; i < segments.length; i++) {
+	    var pos = segments[i];
+	    $(".square[data-pos='" + pos + "']").addClass("snake-body");
+	  }
+	};
+	
+	SnakeView.prototype.step = function () {
+	  this.board.snake.move();
+	  this.render();
 	};
 	
 	
--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ var SnakeView = require("./snake-view.js");
 $(function() {
   var view = new SnakeView($("#snake-game"));
   view.setup();
+  view.eventBinding();
+  view.render();
+
+  setInterval(function(){
+    view.step();
+  }, 500);
 });
